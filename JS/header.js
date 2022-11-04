b888f139af9d61254cd0d451a9d10b3bb640837b
AshlandOrk <110945631+RecomposerAsh@users.noreply.github.com>
2022-11-04 11:32:38 -0400

    fix index.html for deployment

--- JS/header.js
+++ JS/header.js
@@ -1,37 +1,32 @@
 // NAVBAR //
 
 const listItems = [
-    {href: '#', text: 'Logo'},
-    {href: 'home-page.html', text: 'Home'},
-    {href: 'about-page.html', text: 'About'},
-    {href: '#', text: 'Get Involved'},
-    {href: 'resident-stories.html', text: 'Resident Stories'},
-    {href: 'faq.html', text: 'FAQ'},
-    {href: '#', text: 'Support Us'},
-
+  { href: "#", text: "Logo" },
+  { href: "index.html", text: "Home" },
+  { href: "../html/about-page.html", text: "About" },
+  { href: "#", text: "Get Involved" },
+  { href: "../html/resident-stories.html", text: "Resident Stories" },
+  { href: "../html/faq.html", text: "FAQ" },
+  { href: "#", text: "Support Us" },
 ];
 
+const navElement = document.createElement("nav");
+const navList = document.createElement("ul");
+navElement.appendChild(navList);
 
-const navElement = document.createElement("nav")
-const navList = document.createElement("ul")
-navElement.appendChild(navList)
+for (var i = 0; i < listItems.length; i++) {
+  const listItem = document.createElement("li");
+  const itemLink = document.createElement("a");
 
-for (var i = 0; i < listItems.length; i++){
-const listItem = document.createElement("li")
-const itemLink = document.createElement("a")
+  itemLink.href = listItems[i].href;
+  itemLink.innerHTML = listItems[i].text;
 
-itemLink.href = listItems[i].href;
-itemLink.innerHTML = listItems[i].text;
-
-listItem.appendChild(itemLink)
-navList.appendChild(listItem)
+  listItem.appendChild(itemLink);
+  navList.appendChild(listItem);
 }
 
-console.log(navElement)
-
+console.log(navElement);
 
 document.body.appendChild(navElement);
 
 // navList.children[0].className = "current";
-
-
